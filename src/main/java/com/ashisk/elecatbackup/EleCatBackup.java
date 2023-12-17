afca18183b2aa199f2efb52aeef5be1a1b1eab2c
ashisk <lyx1474648596@outlook.com>
2023-12-17 15:22:58 +0800

    11

---
+++ src/main/java/com/ashisk/elecatbackup/EleCatBackup.java
@@ -0,0 +1,297 @@
+package com.ashisk.elecatbackup;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import java.sql.*;
+import java.util.List;
+import java.io.*;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamWriter;
+
+
+import org.bukkit.event.*;
+
+public class EleCatBackup extends JavaPlugin implements Listener{
+    private Connection connection;
+    public void say(String s){
+        CommandSender sender = Bukkit.getConsoleSender();
+        sender.sendMessage(s);
+    }
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        if(!checkDatabaseExists()){
+            createDatabase();
+            createTable();
+        }
+        getServer().getPluginManager().registerEvents(this,this);
+        saveAllPluginInfo();
+        this.saveDefaultConfig();
+        say("§6[§3ECatBackup§6]§7插件成功加载，运行良好!");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        if(connection!=null){
+            try{
+                connection.close();
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+        say("§6[§3ECatBackup§6]§7插件已卸载,但是为什么?");
+    }
+    public Connection getConnection() throws SQLException{
+        if (connection==null||connection.isClosed()){
+            FileConfiguration configurationTemp=getConfig();
+            String mysqlUrl= configurationTemp.getString("info.mysqlUrl");
+            String mysqlUsername=configurationTemp.getString("info.mysqlUser");
+            String mysqlPassword=configurationTemp.getString("info.mysqlAddress");
+            connection=DriverManager.getConnection(mysqlUrl,mysqlUsername,mysqlPassword);
+        }
+        return connection;
+    }
+
+    private boolean checkDatabaseExists(){
+        try{
+            Connection connection=getConnection();
+            Statement statement=connection.createStatement();
+            statement.executeUpdate("use ServerPlugins");
+            statement.close();
+            connection.close();
+            return true;
+        }catch (SQLException e){
+            return false;
+        }
+    }
+    private void createDatabase(){
+        try{
+            Connection connection=getConnection();
+            Statement statement=connection.createStatement();
+            statement.executeUpdate("create database ServerPlugins;");
+            statement.close();
+            connection.close();
+            say("[mysql]成功创建数据库");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+    private void createTable() {
+        try {
+            Connection connection = getConnection();
+            Statement usestatement=connection.createStatement();
+            usestatement.executeUpdate("use ServerPlugins");
+            usestatement.close();
+            PreparedStatement statement = connection.prepareStatement(
+                    "create table plugin_info(" +
+                            "PluginId int(4) not null primary key auto_increment," +
+                            "PluginName varchar(20) unique key  not null," +
+                            "PluginVersion varchar(20)," +
+                            "PluginAuthor varchar(30)," +
+                            "PluginComment varchar(20))");
+            statement.executeUpdate();
+            statement.close();
+            connection.close();
+            say("[mysql]成功创建表");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+    private void savePluginInfo(Plugin plugin){
+        String name=plugin.getName();
+        String version=plugin.getDescription().getVersion();
+        List<String> authors=plugin.getDescription().getAuthors();
+        String author=String.join(",",authors);
+        try{
+            Connection connection=getConnection();
+            Statement usestatement=connection.createStatement();
+            usestatement.executeUpdate("use ServerPlugins");
+            usestatement.close();
+            PreparedStatement statement=connection.prepareStatement(
+                    "insert ignore into plugin_info(PluginName,PluginVersion,Pluginauthor,PluginComment)values (?,?,?,NULL)"
+            );
+            statement.setString(1,name);
+            statement.setString(2,version);
+            statement.setString(3,author);
+            statement.executeUpdate();
+            statement.close();
+            connection.close();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+    private void saveAllPluginInfo(){
+        for (Plugin plugin:Bukkit.getPluginManager().getPlugins()){
+            savePluginInfo(plugin);
+        }
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (label.equalsIgnoreCase("testPlugins")&&sender.hasPermission("elecat.testPlugins")){
+            if(args[0].equalsIgnoreCase("list")){
+                if(!(sender instanceof Player)){
+                    sender.sendMessage("conslse player");
+                }else{
+                    Player player=(Player) sender;
+                    sender.sendMessage("ingame player");
+                    if(sender.isOp()){
+                        sender.sendMessage("admin player");
+                    }else {
+                        sender.sendMessage("common player");
+                    }
+                }
+                try {
+                    Connection connection = getConnection();
+                    Statement statement = connection.createStatement();
+                    Statement usestatement=connection.createStatement();
+                    usestatement.executeUpdate("use ServerPlugins");
+                    usestatement.close();
+                    ResultSet resultSet = statement.executeQuery("SELECT * FROM plugin_info");
+                    sender.sendMessage("插件信息：");
+                    while (resultSet.next()) {
+                        int id=resultSet.getInt("PluginId");
+                        String name = resultSet.getString("PluginName");
+                        String version = resultSet.getString("PluginVersion");
+                        String author = resultSet.getString("PluginAuthor");
+                        String Comment = resultSet.getString("PluginComment");
+                        sender.sendMessage("[ "+id+" ] "+"名称: " + name + ", 版本: " + version + ", 作者: " + author+",   |   备注: " + Comment);
+                    }
+                    resultSet.close();
+                    statement.close();
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                return true;
+            }else if (args[0].equalsIgnoreCase("comment")){
+                try{
+                    Connection connection=getConnection();
+                    Statement usestatement=connection.createStatement();
+                    usestatement.executeUpdate("use ServerPlugins");
+                    usestatement.close();
+                    PreparedStatement statement=connection.prepareStatement(
+                            "update plugin_info set PluginComment=? where PluginID=?"
+                    );
+                    statement.setString(1,args[2]);
+                    statement.setString(2,args[1]);
+                    statement.executeUpdate();
+                    statement.close();
+                    connection.close();
+                }catch (SQLException e){
+                    e.printStackTrace();
+                }
+                say("[EleCat]操作成功!");
+                return true;
+            }else if (args[0].equalsIgnoreCase("admin")){
+                if (args[1].equalsIgnoreCase("delete")){
+                    try{
+                        Connection connection=getConnection();
+                        Statement usestatement=connection.createStatement();
+                        usestatement.executeUpdate("use ServerPlugins");
+                        usestatement.close();
+                        PreparedStatement statement=connection.prepareStatement(
+                                "delete from plugin_info where PluginID=?"
+                        );
+                        statement.setString(1,args[2]);
+                        statement.executeUpdate();
+                        statement.close();
+                        connection.close();
+                    }catch (SQLException e){
+                        e.printStackTrace();
+                    }
+                    say("[EleCat]操作成功!");
+                    return true;
+                }else if (args[1].equalsIgnoreCase("reload")){
+                    try{
+                        Connection connection=getConnection();
+                        Statement usestatement=connection.createStatement();
+                        usestatement.executeUpdate("drop database ServerPlugins");
+                        usestatement.close();
+                        if(!checkDatabaseExists()){
+                            createDatabase();
+                            createTable();
+                            saveAllPluginInfo();
+                        }
+                    }catch (SQLException e){
+                        e.printStackTrace();
+                    }
+                    say("[EleCat]表数据已重载");
+                    return true;
+                }else if (args[1].equalsIgnoreCase("UploadToXml")){
+                    try{
+                    Connection connection = getConnection();
+                    Statement statement = connection.createStatement();
+                    Statement usestatement=connection.createStatement();
+                    usestatement.executeUpdate("use ServerPlugins");
+                    usestatement.close();
+                    ResultSet resultSet = statement.executeQuery("SELECT * FROM plugin_info");
+                    String fileNameXML=args[2];
+                    XMLOutputFactory XMLoutputPlugin=XMLOutputFactory.newInstance();
+                    XMLStreamWriter XMLWritePlugin= XMLoutputPlugin.createXMLStreamWriter(new FileWriter(fileNameXML+".xml"));
+                    XMLWritePlugin.writeStartElement("Plugins");
+                    while (resultSet.next()){
+                        XMLWritePlugin.writeStartElement("ID");
+                        int pluginId=resultSet.getInt("PluginId");
+                        XMLWritePlugin.writeCharacters(String.valueOf(pluginId));
+                        XMLWritePlugin.writeEndElement();
+                        XMLWritePlugin.writeStartElement("Name");
+                        String pluginName=resultSet.getString("PluginName");
+                        if(pluginName!=null){
+                        XMLWritePlugin.writeCharacters(String.valueOf(pluginName));}
+                        XMLWritePlugin.writeEndElement();
+                        XMLWritePlugin.writeStartElement("Version");
+                        String pluginVersion=resultSet.getString("PluginVersion");
+                        if(pluginVersion!=null){
+                        XMLWritePlugin.writeCharacters(String.valueOf(pluginVersion));}
+                        XMLWritePlugin.writeEndElement();
+                        XMLWritePlugin.writeStartElement("Authors");
+                        String pluginAuthor=resultSet.getString("PluginAuthor");
+                        if(pluginAuthor!=null){
+                        XMLWritePlugin.writeCharacters(String.valueOf(pluginAuthor));}
+                        XMLWritePlugin.writeEndElement();
+                        XMLWritePlugin.writeStartElement("Comment");
+                        String pluginComment=resultSet.getString("PluginComment");
+                        if(pluginComment!=null){
+                        XMLWritePlugin.writeCharacters(String.valueOf(pluginComment));}
+                        XMLWritePlugin.writeEndElement();
+                    }XMLWritePlugin.writeEndElement();
+                    XMLWritePlugin.writeEndDocument();
+                    XMLWritePlugin.close();
+                    say("[EleCat]成功将插件数据导出到XML文件");
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                    return true;
+                } else if (args[1].equalsIgnoreCase("openWeb")) {
+                    try{
+                        Connection connection=getConnection();
+                        Statement usestatement=connection.createStatement();
+                        usestatement.executeUpdate("use ServerPlugins");
+                        usestatement.close();
+                        HTMLGenerator htmlGenerator = new HTMLGenerator();
+                        htmlGenerator.setPort(8080);
+                        htmlGenerator.setHostname("localhost");
+                        String tableName = "plugin_info";
+                        String htmlContent = htmlGenerator.generatePluginInfoTable(tableName, connection);
+                        String serverUrl = htmlGenerator.startServerAndReturnURL(htmlContent);
+                        say("Server URL: " + serverUrl);
+                    }catch (SQLException e){
+                        e.printStackTrace();
+                    }
+                    return true;
+                }
+                return true;
+            }
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/ashisk/elecatbackup/HTMLGenerator.java
@@ -0,0 +1,177 @@
+package com.ashisk.elecatbackup;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HTMLGenerator {
+    private Connection connection;
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    private static final int DEFAULT_PORT = 8000;
+    private static final String DEFAULT_HOSTNAME = "localhost";
+    private int port;
+    private String hostname;
+
+    public HTMLGenerator() {
+        this.port = DEFAULT_PORT;
+        this.hostname = DEFAULT_HOSTNAME;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public String generatePluginInfoTable(String tableName, Connection connection) {
+        String htmlContent = generateHTMLTable(connection, tableName);
+        return htmlContent;
+    }
+
+    public String startServerAndReturnURL(String htmlContent) {
+        try {
+            HttpServer server = HttpServer.create(new InetSocketAddress(hostname, port), 0);
+            String serverUrl = "http://" + hostname + ":" + port + "/";
+            server.createContext("/", new HTMLHandler(htmlContent));
+            server.createContext("/update", new UpdateHandler());
+            server.setExecutor(null);
+            server.start();
+            return serverUrl;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private String generateHTMLTable(Connection connection, String tableName) {
+        StringBuilder htmlBuilder = new StringBuilder();
+        htmlBuilder.append("<html><body><table>");
+
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM " + tableName);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                String pluginName = resultSet.getString("PluginName");
+                String pluginVersion = resultSet.getString("PluginVersion");
+                int pluginId = resultSet.getInt("PluginId");
+                String pluginAuthor = resultSet.getString("PluginAuthor");
+                String pluginComment = resultSet.getString("PluginComment");
+                htmlBuilder.append("<tr><td>").append(pluginId).append("</td><td>").append(pluginName)
+                        .append("</td><td>").append(pluginVersion).append("</td><td>").append(pluginAuthor)
+                        .append("</td><td><input type='text' value='").append(pluginComment)
+                        .append("' onchange='updateComment(this.value, ").append(pluginId).append(")'></td></tr>");
+            }
+            resultSet.close();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        htmlBuilder.append("</table></body><script>\n" +
+                "function updateComment(comment, pluginId) {\n" +
+                "  var xhr = new XMLHttpRequest();\n" +
+                "  xhr.open('POST', '/update', true);\n" +
+                "  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');\n" +
+                "  xhr.onreadystatechange = function() {\n" +
+                "    if (xhr.readyState === 4 && xhr.status === 200) {\n" +
+                "      console.log('Comment updated successfully');\n" +
+                "    }\n" +
+                "  };\n" +
+                "  xhr.send('pluginId=' + pluginId + '&pluginComment=' + encodeURIComponent(comment));\n" +
+                "}\n" +
+                "</script></html>");
+
+        return htmlBuilder.toString();
+    }
+
+    static class HTMLHandler implements HttpHandler {
+        private final String htmlContent;
+
+        public HTMLHandler(String htmlContent) {
+            this.htmlContent = htmlContent;
+        }
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            exchange.getResponseHeaders().set("Content-Type", "text/html");
+            exchange.sendResponseHeaders(200, htmlContent.getBytes().length);
+            OutputStream outputStream = exchange.getResponseBody();
+            outputStream.write(htmlContent.getBytes());
+            outputStream.close();
+        }
+    }
+
+    class UpdateHandler implements HttpHandler {
+        private Connection connection;
+
+        public void setConnection(Connection connection) {
+            this.connection = connection;
+        }
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            if (exchange.getRequestMethod().equalsIgnoreCase("POST")) {
+                String requestBody = new String(exchange.getRequestBody().readAllBytes());
+                Map<String, String> requestData = parseFormData(requestBody);
+                String comment = requestData.get("pluginComment");
+                int pluginId = Integer.parseInt(requestData.get("pluginId"));
+                updatePluginComment(pluginId, comment);
+                String response = "Comment updated successfully";
+                exchange.getResponseHeaders().set("Content-Type", "text/plain");
+                exchange.sendResponseHeaders(200, response.getBytes().length);
+                OutputStream outputStream = exchange.getResponseBody();
+                outputStream.write(response.getBytes());
+                outputStream.close();
+            } else {
+                String response = "Invalid request";
+                exchange.sendResponseHeaders(405, response.getBytes().length);
+                OutputStream outputStream = exchange.getResponseBody();
+                outputStream.write(response.getBytes());
+                outputStream.close();
+            }
+        }
+
+        private Map<String, String> parseFormData(String formData) {
+            Map<String, String> data = new HashMap<>();
+            String[] pairs = formData.split("&");
+            for (String pair : pairs) {
+                String[] keyValue = pair.split("=");
+                if (keyValue.length == 2) {
+                    String key = keyValue[0];
+                    String value = keyValue[1];
+                    data.put(key, value);
+                }
+            }
+            return data;
+        }
+
+        public void updatePluginComment(int pluginId, String comment) {
+            try {
+                PreparedStatement statement = connection.prepareStatement("UPDATE plugin_info SET PluginComment = ? WHERE PluginId = ?");
+                statement.setString(1, comment);
+                statement.setInt(2, pluginId);
+                statement.executeUpdate();
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
